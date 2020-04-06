59decb9b58d4e4c44f230a197c336a6105287425
Leila <nb.leila10@gmail.com>
2020-04-05 20:52:51 -0500

    Use named route-view to play with the layout

--- src/App.vue
+++ src/App.vue
@@ -4,21 +4,16 @@
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
         <h1>Routing</h1>
         <hr />
-        <Header />
+        <router-view name="header-top" />
         <router-view />
+        <router-view name="header-bottom" />
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import Header from "./components/Header.vue";
-
-export default {
-  components: {
-    Header,
-  },
-};
+export default {};
 </script>
 
 <style></style>
--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,24 @@
 import Home from "./components/Home.vue";
+import Header from "./components/Header.vue";
 import User from "./components/user/User.vue";
 import UserStart from "./components/user/UserStart.vue";
 import UserDetail from "./components/user/UserDetail.vue";
 import UserEdit from "./components/user/UserEdit.vue";
 
 export const routes = [
-  { path: "", component: Home },
+  {
+    path: "",
+    components: {
+      default: Home,
+      "header-top": Header,
+    },
+  },
   {
     path: "/user",
-    component: User,
+    components: {
+      default: User,
+      "header-bottom": Header,
+    },
     children: [
       { path: "", component: UserStart },
       { path: ":id", component: UserDetail, props: true },
