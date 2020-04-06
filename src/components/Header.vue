a523653e8321124e4b096bae92fb2187f87e9db1
Leila <nb.leila10@gmail.com>
2020-04-05 19:28:08 -0500

    Add Header component to add the home and user routes

---
+++ src/components/Header.vue
@@ -0,0 +1,14 @@
+<template>
+  <ul class="nav nav-pills">
+    <li class="nav-item">
+      <router-link to="/">Home</router-link>
+    </li>
+    <li class="nav-item">
+      <router-link to="/user">User</router-link>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {};
+</script>
