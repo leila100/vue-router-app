5e06af78fc50985eea8579eca2541c8bc5a26a54
Leila <nb.leila10@gmail.com>
2020-04-06 17:04:00 -0500

    Group all the lazy loading into one package called user

--- src/routes.js
+++ src/routes.js
@@ -6,24 +6,40 @@ import Header from "./components/Header.vue";
 // import UserEdit from "./components/user/UserEdit.vue";
 
 const User = (resolve) => {
-  require.ensure(["./components/user/User.vue"], () => {
-    resolve(require("./components/user/User.vue"));
-  });
+  require.ensure(
+    ["./components/user/User.vue"],
+    () => {
+      resolve(require("./components/user/User.vue"));
+    },
+    "user"
+  );
 };
 const UserStart = (resolve) => {
-  require.ensure(["./components/user/UserStart.vue"], () => {
-    resolve(require("./components/user/UserStart.vue"));
-  });
+  require.ensure(
+    ["./components/user/UserStart.vue"],
+    () => {
+      resolve(require("./components/user/UserStart.vue"));
+    },
+    "user"
+  );
 };
 const UserDetail = (resolve) => {
-  require.ensure(["./components/user/UserDetail.vue"], () => {
-    resolve(require("./components/user/UserDetail.vue"));
-  });
+  require.ensure(
+    ["./components/user/UserDetail.vue"],
+    () => {
+      resolve(require("./components/user/UserDetail.vue"));
+    },
+    "user"
+  );
 };
 const UserEdit = (resolve) => {
-  require.ensure(["./components/user/UserEdit.vue"], () => {
-    resolve(require("./components/user/UserEdit.vue"));
-  });
+  require.ensure(
+    ["./components/user/UserEdit.vue"],
+    () => {
+      resolve(require("./components/user/UserEdit.vue"));
+    },
+    "user"
+  );
 };
 
 export const routes = [
