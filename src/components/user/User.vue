905d6168d367009afdb1aa98027a1b6e679054d2
Leila <nb.leila10@gmail.com>
2020-04-05 20:16:29 -0500

    Add basic nested routes in the user page

--- src/components/user/User.vue
+++ src/components/user/User.vue
@@ -1,14 +1,14 @@
 <template>
   <div>
     <h1>The User Page</h1>
-    <p>Loaded ID: {{ id }}</p>
     <button @click="navigateToHome" class="btn btn-primary">Go Home</button>
+    <hr />
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
 export default {
-  props: ["id"],
   methods: {
     navigateToHome() {
       this.$router.push("/");
