2dc58e068172269b7df91b9b2971148f0870099d
Leila <nb.leila10@gmail.com>
2020-04-05 20:36:58 -0500

    Make routes more dynamic

--- src/components/user/UserDetail.vue
+++ src/components/user/UserDetail.vue
@@ -2,6 +2,8 @@
   <div>
     <h3>Some User Details</h3>
     <p>User loaded has id: {{ id }}</p>
+    <hr />
+    <router-link :to="`/user/${id}/edit`" class="btn btn-primary">Edit User</router-link>
   </div>
 </template>
 
