16a3b5a2cab181e2c6764c275eaa0b45f2f51a51
Leila <nb.leila10@gmail.com>
2020-04-06 16:52:29 -0500

    Use beforeRouteLeave to confirm that the user is ready to leave the page

--- src/components/user/UserEdit.vue
+++ src/components/user/UserEdit.vue
@@ -5,6 +5,23 @@
     <p>Analytics: {{ $route.query.q }}</p>
     <!-- <div style="height: 1700px;"></div> -->
     <p id="data">Some data</p>
-    <button class="btn btn-danger">Confirm</button>
+    <button class="btn btn-danger" @click="confirmed = true">Confirm</button>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      confirmed: false
+    };
+  },
+  beforeRouteLeave(to, from, next) {
+    if (this.confirmed) next();
+    else {
+      if (confirm("Are you sure!?")) next();
+      else next(false);
+    }
+  }
+};
+</script>
