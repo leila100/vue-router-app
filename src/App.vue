8ed425d7af52aa3e557fa120190b0b429b521939
Leila <nb.leila10@gmail.com>
2020-04-05 21:11:11 -0500

    Add some transition animation between routes

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,9 @@
         <h1>Routing</h1>
         <hr />
         <router-view name="header-top" />
-        <router-view />
+        <transition name="slide" mode="out-in">
+          <router-view />
+        </transition>
         <router-view name="header-bottom" />
       </div>
     </div>
@@ -16,4 +18,35 @@
 export default {};
 </script>
 
-<style></style>
+<style>
+.slide-leave-active {
+  transition: opacity 1s ease;
+  opacity: 0;
+  animation: slide-out 1s ease-out forwards;
+}
+.slide-leave {
+  opacity: 1;
+  transform: translateX(0);
+}
+.slide-enter-active {
+  animation: slide-in 1s ease-out forwards;
+}
+
+@keyframes slide-out {
+  0% {
+    transform: translateY(0);
+  }
+  100% {
+    transform: translateY(-30px);
+  }
+}
+
+@keyframes slide-in {
+  0% {
+    transform: translateY(-30px);
+  }
+  100% {
+    transform: translateY(0);
+  }
+}
+</style>
--- src/components/user/User.vue
+++ src/components/user/User.vue
@@ -3,7 +3,9 @@
     <h1>The User Page</h1>
     <button @click="navigateToHome" class="btn btn-primary">Go Home</button>
     <hr />
-    <router-view></router-view>
+    <transition name="slide" mode="out-in">
+      <router-view></router-view>
+    </transition>
   </div>
 </template>
 
@@ -16,3 +18,36 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+.slide-leave-active {
+  transition: opacity 1s ease;
+  opacity: 0;
+  animation: slide-out 1s ease-out forwards;
+}
+.slide-leave {
+  opacity: 1;
+  transform: translateX(0);
+}
+.slide-enter-active {
+  animation: slide-in 1s ease-out forwards;
+}
+
+@keyframes slide-out {
+  0% {
+    transform: translateY(0);
+  }
+  100% {
+    transform: translateY(-30px);
+  }
+}
+
+@keyframes slide-in {
+  0% {
+    transform: translateY(-30px);
+  }
+  100% {
+    transform: translateY(0);
+  }
+}
+</style>
