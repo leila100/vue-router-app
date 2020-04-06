458a2a93b0a95f23f7b135e005483d64709c47d1
Leila <nb.leila10@gmail.com>
2020-04-05 19:43:09 -0500

    Add navigation using router.push

--- src/components/user/User.vue
+++ src/components/user/User.vue
@@ -1,4 +1,16 @@
 <template>
+  <div>
     <h1>The User Page</h1>
-
+    <button @click="navigateToHome" class="btn btn-primary">Go Home</button>
+  </div>
 </template>
+
+<script>
+export default {
+  methods: {
+    navigateToHome() {
+      this.$router.push("/");
+    },
+  },
+};
+</script>
