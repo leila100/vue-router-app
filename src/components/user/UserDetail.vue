35e25824ff06f2dbf5cd3dcfffef394da75d7fe8
Leila <nb.leila10@gmail.com>
2020-04-05 20:45:10 -0500

    Set and access query parameters

--- src/components/user/UserDetail.vue
+++ src/components/user/UserDetail.vue
@@ -3,7 +3,9 @@
     <h3>Some User Details</h3>
     <p>User loaded has id: {{ id }}</p>
     <hr />
-    <router-link :to="{ name: 'userEdit', params: { id: id } }" class="btn btn-primary">Edit User</router-link>
+    <router-link :to="{ name: 'userEdit', params: { id: id }, query: { local: 'en', q: 100 } }" class="btn btn-primary"
+      >Edit User</router-link
+    >
   </div>
 </template>
 
